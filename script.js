feb433cf10f0e737f63c5cc427bd440e12f4f51a
Andriyivna <vika.herman.97@gmail.com>
2020-04-02 19:07:44 +0200

    add script.js file

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+	let textElement = document.querySelector('#main-text');
+	let steveJobsText ='Steve Jobs';
+	console.log(steveJobsText);
+	console.log(textElement);
+
+});
