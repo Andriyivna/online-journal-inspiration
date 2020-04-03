b1ccc64db4d522635d9db44553e483c4ae0d8bd9
Andriyivna <vika.herman.97@gmail.com>
2020-04-03 22:27:38 +0200

    Add js scroll, add animation for text , add page about sheryl

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
 	let textElement = document.querySelector('#main-text');
-	let steveJobsText ='Steve Jobs';
-	console.log(steveJobsText);
+	let Text = textElement.innerHTML;
+
+	textElement.innerHTML = '';
+	
 	console.log(textElement);
+	let six = 0;
+
+
+ function repeatingFunc() {		
+ 	if (six==Text.length){
+ 		return;
+ 	}	    
+		textElement.innerHTML += Text[six]; 
+		six++;
+		
+	    setTimeout(repeatingFunc, 250); 		
+		
+	} 
+	setTimeout(repeatingFunc, 250);
 
 });
---
+++ scroll-to-top-script.js
@@ -0,0 +1,17 @@
+//Get the button:
+ let mybutton = document.querySelector('.top-button');
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0; 
+}
